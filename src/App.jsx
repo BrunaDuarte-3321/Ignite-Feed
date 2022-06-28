f2f41a8b0a804465f38ea2a6ebe22efba7f5938b
Bruna Duarte <b.elisabete.duarte@parvi.com.br>
2022-06-28 19:27:27 -0300

    Adicionando componente sidebar da aplicação.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { Post } from "./Post"
+import styles from './App.module.css';
 import { Header } from "./components/Header"
+import { Sidebar } from "./components/Sidebar";
 
 export const  App = () =>  {
   return (
     <>
       <Header/>
-      <h1>Hello world!</h1>
-      <Post author='Criado por Bruna Duarte' data='28/06/2022' />
-      <Post author='Criado por Ana Duarte' data='02/07/2022' />
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+          <Post author='Criado por Bruna Duarte' data='28/06/2022' />
+          <Post author='Criado por Ana Duarte' data='02/07/2022' />
+        </main>
+      </div>
+
     </>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,22 @@
+import styles from '../styles/Sidebar.module.css'
+
+export const Sidebar = () => {
+  return (
+    <aside className={styles.sidebar}>
+      <img className={styles.cover}
+        src="https://images.unsplash.com/photo-1592609931095-54a2168ae893?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" alt=""
+      />
+      <div className={styles.profile}>
+        <strong>Bruna Duarte</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          Editar seu perfil
+        </a>
+      </footer>
+
+    </aside>
+  )
+}
